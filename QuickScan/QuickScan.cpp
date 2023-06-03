848bf4e058ccf0e0927c510632772cbd73404e9e
pawelwuuu <wojcikp376@gmail.com>
2023-06-03 12:53:17 +0200

    project is able to read scanned text

--- QuickScan/QuickScan.cpp
+++ QuickScan/QuickScan.cpp
@@ -1,21 +1,71 @@
 ﻿#include "ScreenSelector.h"
 #include "ScreenshotProvider.h"
+#include <iostream>
+#include <Windows.h>  // Do obsługi schowka Windows
+#include <fstream>    // Do zapisu obrazu jako pliku tymczasowego
+#include <tesseract/baseapi.h>  // Do rozpoznawania tekstu z obrazu
+#include <leptonica/allheaders.h>
+#include <cstdio>
 
-using namespace std;
+#pragma comment (lib, "crypt32.lib")
 
+const char* SCREENSHOT_FILENAME = "screenshot.bmp";
 
+void SetTextToClipboard(const std::string& text)
+{
+    if (!OpenClipboard(NULL))
+    {
+        // Obsłuż błąd
+        return;
+    }
+
+    EmptyClipboard();
+
+    int textLength = static_cast<int>(text.length());
+    HGLOBAL hText = GlobalAlloc(GMEM_MOVEABLE, (textLength + 1) * sizeof(char));
+    if (hText != NULL)
+    {
+        char* pText = static_cast<char*>(GlobalLock(hText));
+        strcpy_s(pText, textLength + 1, text.c_str());
+        GlobalUnlock(hText);
+
+        SetClipboardData(CF_TEXT, hText);
+    }
+
+    CloseClipboard();
+}
 
 int main(int argc, char* argv[]) {
 
     Cordinates cordinates = getCordinates();
-    cout << "Cordinates are: " << endl;
-    cout << cordinates.x << endl;
-    cout << cordinates.y << endl;
-    cout << cordinates.width << endl;
-    cout << cordinates.height << endl;
 
-    GetScreenShot(cordinates.x, cordinates.y, cordinates.width, cordinates.height);
+    CreateScreenshot(cordinates.x, cordinates.y, cordinates.width, cordinates.height, SCREENSHOT_FILENAME);
+    
+    tesseract::TessBaseAPI* api = new tesseract::TessBaseAPI();
 
+
+    // Initialize tesseract-ocr with English, without specifying tessdata path
+    if (api->Init(NULL, "pol")) {
+        fprintf(stderr, "Could not initialize tesseract.\n");
+        exit(1);
+    }
+
+    // Open input image with leptonica library
+    Pix* image = pixRead(SCREENSHOT_FILENAME);
+    api->SetImage(image);
+    
+    char* outText = api->GetUTF8Text();
+    printf("%s", outText);
+    std::string ocrResult(outText);
+
+    api->End();
+    delete api;
+    delete[] outText;
+    pixDestroy(&image);
+
+    SetTextToClipboard(ocrResult);
+
+    remove(SCREENSHOT_FILENAME);
 
     return 0;
 }
--- QuickScan/screenshotprovider.h
+++ QuickScan/screenshotprovider.h
@@ -7,24 +7,71 @@
 
 #endif //UNTITLED_SCREENSHOTPROVIDER_H
 
-#include <windows.h>
-
-void GetScreenShot(int x, int y, int width, int height)
+void SaveHBITMAPToFile(HBITMAP hBitmap, int width, int height, const char* filename)
 {
-    // copy screen to bitmap
-    HDC     hScreen = GetDC(NULL);
-    HDC     hDC = CreateCompatibleDC(hScreen);
+    BITMAPINFO bmi = { 0 };
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = width;
+    bmi.bmiHeader.biHeight = -height;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 24; // Assuming 24-bit RGB format
+    bmi.bmiHeader.biCompression = BI_RGB;
+    bmi.bmiHeader.biSizeImage = 0;
+    bmi.bmiHeader.biXPelsPerMeter = 0;
+    bmi.bmiHeader.biYPelsPerMeter = 0;
+    bmi.bmiHeader.biClrUsed = 0;
+    bmi.bmiHeader.biClrImportant = 0;
+
+    // Create file
+    HANDLE file = CreateFileA(filename, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (file == INVALID_HANDLE_VALUE)
+    {
+        // Error handling
+        return;
+    }
+
+    // Write BITMAPFILEHEADER
+    BITMAPFILEHEADER bmfHeader;
+    bmfHeader.bfType = 0x4D42; // "BM"
+    DWORD dwSizeImage = ((width * 24 + 31) & ~31) / 8 * height; // Calculate image size
+    bmfHeader.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwSizeImage;
+    bmfHeader.bfReserved1 = 0;
+    bmfHeader.bfReserved2 = 0;
+    bmfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+    DWORD dwBytesWritten;
+    WriteFile(file, &bmfHeader, sizeof(BITMAPFILEHEADER), &dwBytesWritten, NULL);
+    WriteFile(file, &(bmi.bmiHeader), sizeof(BITMAPINFOHEADER), &dwBytesWritten, NULL);
+
+    // Get bitmap data
+    BYTE* pBits = new BYTE[dwSizeImage];
+    HDC hDC = CreateCompatibleDC(NULL);
+    HGDIOBJ hOld = SelectObject(hDC, hBitmap);
+    GetDIBits(hDC, hBitmap, 0, height, pBits, &bmi, DIB_RGB_COLORS);
+
+    // Write bitmap data
+    WriteFile(file, pBits, dwSizeImage, &dwBytesWritten, NULL);
+
+    // Clean up
+    SelectObject(hDC, hOld);
+    DeleteDC(hDC);
+    CloseHandle(file);
+    delete[] pBits;
+}
+
+void CreateScreenshot(int x, int y, int width, int height, const char* filename)
+{
+    // Copy screen to bitmap
+    HDC hScreen = GetDC(NULL);
+    HDC hDC = CreateCompatibleDC(hScreen);
     HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, width, height);
     HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
-    BOOL    bRet = BitBlt(hDC, 0, 0, width, height, hScreen, x, y, SRCCOPY);
+    BOOL bRet = BitBlt(hDC, 0, 0, width, height, hScreen, x, y, SRCCOPY);
 
-    // save bitmap to clipboard
-    OpenClipboard(NULL);
-    EmptyClipboard();
-    SetClipboardData(CF_BITMAP, hBitmap);
-    CloseClipboard();
+    // Save bitmap to file
+    SaveHBITMAPToFile(hBitmap, width, height, filename);
 
-    // clean up
+    // Clean up
     SelectObject(hDC, old_obj);
     DeleteDC(hDC);
     ReleaseDC(NULL, hScreen);
